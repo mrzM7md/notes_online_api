68a5d8f2e7c9b10f0fa3e6dc4e33707d77037cb9
medo123 <mrz.m7md@gmail.com>
2024-02-21 01:30:04 +0300

    adds

--- auth/login.php
+++ auth/login.php
@@ -8,12 +8,14 @@ $password = filterPostRequest(requestName: "password");
 $statment = $con->
                     prepare('SELECT * FROM `users` WHERE (`username` = ? OR `email` = ?) AND `password` = ? ');
 
-    $statment->execute(array($username, $username, $password));
+$statment->execute(array($username, $username, $password));
+
+$data = $statment->fetch(PDO::FETCH_ASSOC);
 
 $count = $statment->rowCount();
 
 if($count > 0){
-    echo json_encode(array("status" => "success"));
+    echo json_encode(array("status" => "success", "data" => $data));
 }
 else{
     echo json_encode(array("status" => "fail"));
---
+++ notes/view.php
@@ -0,0 +1,22 @@
+<?php
+
+include "../connect.php";
+
+$user_id = $_GET['user'];
+
+$statment = $con-> prepare('SELECT * FROM `notes` WHERE `user_id` = ?');
+
+$statment->execute(array($user_id));
+
+$data = $statment->fetchAll(PDO::FETCH_ASSOC);
+
+$count = $statment->rowCount();
+
+if($count > 0){
+    echo json_encode(array("status" => "success", "data" => $data));
+}
+else{
+    echo json_encode(array("status" => "fail"));
+}
+
+?>
